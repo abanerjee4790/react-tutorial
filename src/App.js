acb6199496e2e4f1a45bc5aec74abffd15fd29de
abbanerjee <abhishek.d.banerjee@capgemini.com>
2020-09-07 19:50:16 +0530

    Component Part 2

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
     return (
       <div>
@@ -12,7 +20,7 @@ class App extends React.Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu/>
+        <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
