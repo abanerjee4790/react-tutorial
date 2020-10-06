8af2a7586cae0222d4bc8851c60639b83535f35d
abbanerjee <abhishek.d.banerjee@capgemini.com>
2020-10-06 10:35:38 +0530

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,47 +1,55 @@
 import React from 'react';
 import Menu from './MenuComponent';
-import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends React.Component {
     constructor(props) {
-      super(props);
+        super(props);
 
-      this.state = {
-        selectedDish: null,
-        dishes: DISHES
-      };
+        this.state = {
+            dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
+        };
     }
 
     render() {
-      const HomePage = () => {
-        return(
-            <Home />
-        );
-      }
+        const HomePage = () => {
+            return (
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                leader={this.state.leaders.filter((leader) => leader.featured)[0]}/>
+            );
+        }
 
-      const MenuPage = () => {
+        const MenuPage = () => {
+            return (
+                <Menu dishes={this.state.dishes} />
+            );
+        }
+
         return (
-          <Menu dishes={this.state.dishes} />
+            <div>
+                <Header />
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/menu' component={MenuPage} />
+                    <Route exact path='/contactus' component={Contact} />
+                    <Redirect to="/home" />
+                </Switch>
+                <Footer />
+            </div>
         );
-      }
-
-      return (
-        <div>
-          <Header />
-          <Switch>
-              <Route path='/home' component={HomePage} />
-              <Route exact path='/menu' component={MenuPage} />
-              <Redirect to="/home" />
-          </Switch>
-          <Footer />
-        </div>
-      );
     }
-  }
-  
-  export default Main;
+}
+
+export default Main;
